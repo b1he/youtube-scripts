01cd64daae8c3fbbb9a2f870ed730578c466bf8c
b1he <3150824+b1he@users.noreply.github.com>
2020-01-09 23:46:09 +1100

    update everything
    gitignore
    readme
    autogen music
    move lut

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # youtube-scripts
 Some YouTube scripts for experiments.
+
+### youtube-dl scripts
+Useful to automate youtube-dl tasks.
+* download - 4k: Download the 4K VP9 SDR video.
+
+* download - 4khdr: Download the 4K VP9 HDR video.
+
+* download - best best: Download the best video and audio stream according to youtube-dl and combine. Does not always choose the best combination such as VP9 video and AAC audio.
+
+* info - loop: Checks every 60 seconds the different encodes available on YouTube. Useful for checking if a higher resolution encode has been processed.
+
+* info + download no.: Returns the encode ids available to download which is entered. e.g: 337+251
+
+* info: Lists different encodes available.
+
+* update: Updates youtube-dl.
+
+### youtube-auto-generated-music-downloader
+Script to automatically download YouTube auto-generated music with tagging and album cover.
+Requires youtube-dl, ffmpeg and tageditor.
+One of my first projects that actually does something (not just a hello world one.)
+
+#### Instructions
+Run the batch file and paste the YouTube auto-generated music link there (ctrl+v if on Windows 10 or right click with your mouse).
+
+You can also edit the batch file to change some parameters:
+* musicdir: The folder where the music will be stored.
+* format: opus50k, opus70k, opus160k, m4a128k, m4a192k or vorbis128k
+* bin: The location of the binary folder.
+* skipdownloadercover: whether to skip downloading cover or not.
+
+### yt upscale and upload
+An experiment with various strategies to 'increase the bitrate' of videos uploaded to YouTube. Useful for archiving non-critical footage in high quality like hours of gameplay!
---
+++ yt autogen music dl/start.bat
@@ -0,0 +1,64 @@
+@echo off
+title YouTube auto-generated music downloader with tagging and cover art
+echo YouTube auto-generated music downloader with tagging and cover art
+
+set musicdir=Music\
+set bin=bin\
+set format=m4a128k
+set skipdownloadcover=no
+
+if %skipdownloadcover%==no set coverid=,248
+if %format%==opus50k set id=249&set extension=opus&goto download
+if %format%==opus70k set id=250&set extension=opus&goto download
+if %format%==opus160k set id=251&set extension=opus&goto download
+if %format%==m4a128k set id=140&set extension=m4a&goto download
+if %format%==vorbis128k set id=171&set extension=ogg&goto download
+rem There's no more 192k aac on YouTube?
+
+echo Please edit this file and choose a correct format: opus50k, opus70k, opus160k, m4a128k or vorbis128k
+pause
+exit /b
+
+:download
+set /p url=Enter URL:
+
+echo Checking link...
+%bin%youtube-dl -s %url% -q >NUL
+if %errorlevel%==1 echo There was a problem retrieving the video. Check that the link is correct.&pause&exit /b
+if not exist "%musicdir%" mkdir %musicdir%
+
+echo Getting title and artist...
+for /f "delims=" %%a in ('%bin%youtube-dl --get-filename -o "%%(title)s" "%url%"') do @set songtitle=%%a
+for /f "delims=" %%a in ('%bin%youtube-dl --get-filename -o "%%(uploader)s" "%url%"') do @set uploader=%%a
+if not "%uploader%"=="%uploader: - Topic=%" goto pass
+echo Is this a YouTube auto-generated music link?&pause&exit /b
+:pass
+set uploader=%uploader: - Topic=%
+set filename=%uploader% - %songtitle%
+echo %filename%
+if "%uploader%"==Various Artists goto set /p uploader=Please enter the main artist's name (Because unfortunately the uploader is called 'Various Artists'):
+if %skipdownloadcover%==no echo Downloading song and cover...&goto withcover
+echo Downloading song...
+:withcover
+%bin%youtube-dl.exe -f %id%%coverid% -o "%musicdir%%filename%.%%(ext)s" %url% -x -q 2>NUL
+if %skipdownloadcover%==yes goto skipcover
+
+echo Extracting album art...
+%bin%ffmpeg -loglevel panic -i "%musicdir%%filename%.webm" -ss 00:00:06 -vf crop=732:732:110:108 -q:v 0 -vframes 1 "%musicdir%art.jpg" 2>NUL
+%bin%tageditor set title="%songtitle%" artist="%uploader%" cover="%musicdir%art.jpg" --files "%musicdir%%filename%.%extension%"
+del "%musicdir%%filename%.webm"
+del "%musicdir%%filename%.*.bak
+del "%musicdir%art.jpg"
+
+
+echo Finished!
+pause
+exit /b
+
+:skipcover
+%bin%tageditor set title="%songtitle%" artist="%uploader%" "%musicdir%%filename%.%extension%" 1>NUL
+del "%musicdir%%filename%.*.bak
+
+echo Finished!
+pause
+exit /b
